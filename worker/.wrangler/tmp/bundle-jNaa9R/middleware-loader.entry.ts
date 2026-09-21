c3e221ca393fd7358a995d86543588d8383dfcb4
unknown <yanglin@deye.com.cn>
2026-09-21 11:26:29 +0800

    Fixed some worker issues

--- worker/.wrangler/tmp/bundle-jNaa9R/middleware-loader.entry.ts
+++ worker/.wrangler/tmp/bundle-jNaa9R/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-t5MYl2\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-jNaa9R\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-t5MYl2\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-jNaa9R\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-t5MYl2\\middleware-insertion-facade.js";
+export * from "D:\\Work-DIR\\97_pythonSample\\04_VsCode\\Gotit\\worker\\.wrangler\\tmp\\bundle-jNaa9R\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
